1feb1eb4d35f92fff18f9f3a46d2c8a5c042235f
luan.daros <darosluan@gmail.com>
2024-11-05 07:42:44 -0300

    feature: package as lib

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,39 @@
-console.log("Hello world!");
+import {
+    Message,
+    IModel,
+    ILogger,
+    IParser,
+    ITool,
+    ModelOutput,
+    AgentOutput,
+    ExecutionParams,
+    AgentMetadata,
+    ModelMetadata,
+    ToolExecutionResult,
+    ToolExecutorOutput,
+} from "./core/interfaces";
+
+import { InitI18n } from "./i18n";
+import { Agent } from "./core/agent";
+import { ToolExecutor } from "./core/tool-executor";
+
+import { OpenAIModel } from "./models/open-ai";
+
+export {
+    Agent,
+    ToolExecutor,
+    InitI18n,
+    Message,
+    IModel,
+    ILogger,
+    IParser,
+    ITool,
+    ModelOutput,
+    AgentOutput,
+    ExecutionParams,
+    AgentMetadata,
+    ModelMetadata,
+    ToolExecutionResult,
+    ToolExecutorOutput,
+    OpenAIModel,
+};
